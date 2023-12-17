1b73fad899595fd30fd795386c26ec33884b608c
SrGeneroso <5541794+SrGeneroso@users.noreply.github.com>
2023-12-17 23:06:57 +0100

    objecto clientes

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -4,11 +4,13 @@ export const actions = {
 		const sessionID = await fetchSession();
 		return await fetchCredentials(data, sessionID);
 	},
-	createClient: async ({ request }) => {
+	insertClient: async ({ request }) => {
 		const data = await request.formData();
 		const auth = data.get('auth');
-		const clientID = await fetchNewClientID(auth);
-		return { cookie: auth, clientID: clientID };
+		const clientData = JSON.parse(data.get('clientList'));
+		const processedClients = await insertClient(auth, clientData);
+
+		return { cookie: auth, processedClients: processedClients };
 	}
 };
 
@@ -92,49 +94,51 @@ async function fetchCredentials(data, sessionID) {
 	}
 }
 
-async function fetchNewClientID(auth) {
+//Do I create a user and then populate the data or do I create the user at once? To post you need the client id
+async function createNewClient(auth, client) {
 	const url = 'https://app.gdtaller.com/app/app_contactos_form.php?cod=-1&tipo=-1';
-	const form = new FormData();
-	form.append('hidGuardado', 'o');
-	form.append('eCodigo', '-1');
-	form.append('BtnCrear_btn', 'Crear Contacto');
-	form.append('BtnCrear', 'Crear Contacto');
-	form.append('selecInstalacion', '2124');
-	form.append('selecComercial', '-1');
-	form.append('hidIdTipoSelec', '-1');
-	form.append('txtNombre', 'test');
-	form.append('selecTarifaDef', '-1');
-	form.append('hidTab', '1');
-	form.append('ckbTipo_0', '24');
-	form.append('eNumChecks', '3');
-	form.append('selecSector', '-1');
-	form.append('hidVariasActividades', 'si');
-	form.append('eNumActividades', '0');
-	form.append('ckbRecibirPublicidad', 'on');
-	form.append('txtWeb', 'http://');
-	form.append('selecPais', '61');
-	form.append('selecProvincia', '-1');
-	form.append('ckbUsarGMap', 'on');
-	form.append('hidUsarGMap', '0');
-	form.append('hidGMapZoom', '0');
-	form.append('hidGMapAsignar', 'si');
-	form.append('hidGMapLat', '0');
-	form.append('hidGMapLong', '0');
-	form.append('selecPaisEntrega', '61');
-	form.append('selecProvinciaEntrega', '-1');
-	form.append('radioPostalTitular', '1');
-	form.append('radioPostalDomicilio', '1');
-	form.append('selecFormaPago', '-1');
-	form.append('selecVencimiento', '-1');
-	form.append('selecDiaPago', '0');
-	form.append('selecDiaPago2', '0');
-	form.append('hidEstadoCcc', '3');
-	form.append('radioIbanCompleto', '0');
-	form.append('txtValorDcto_0', '0,00');
-	form.append('txtValorDcto_1', '0,00');
-	form.append('hidNumChecksDctos', '2');
-	form.append('txtOrden', '1');
-	form.append('ckbVisible', '1');
+	const formData = new URLSearchParams([
+		['hidGuardado', 'o'],
+		['eCodigo', '-1'],
+		['BtnCrear_btn', 'Crear Contacto'],
+		['BtnCrear', 'Crear Contacto'],
+		['selecInstalacion', '2124'],
+		['selecComercial', '-1'],
+		['hidIdTipoSelec', '-1'],
+		['txtNombre', 'test'],
+		['selecTarifaDef', '-1'],
+		['hidTab', '1'],
+		['ckbTipo_0', '24'],
+		['eNumChecks', '3'],
+		['selecSector', '-1'],
+		['hidVariasActividades', 'si'],
+		['eNumActividades', '0'],
+		['ckbRecibirPublicidad', 'on'],
+		['txtWeb', 'http://'],
+		['selecPais', '61'],
+		['selecProvincia', '-1'],
+		['ckbUsarGMap', 'on'],
+		['hidUsarGMap', '0'],
+		['hidGMapZoom', '0'],
+		['hidGMapAsignar', 'si'],
+		['hidGMapLat', '0'],
+		['hidGMapLong', '0'],
+		['selecPaisEntrega', '61'],
+		['selecProvinciaEntrega', '-1'],
+		['radioPostalTitular', '1'],
+		['radioPostalDomicilio', '1'],
+		['selecFormaPago', '-1'],
+		['selecVencimiento', '-1'],
+		['selecDiaPago', '0'],
+		['selecDiaPago2', '0'],
+		['hidEstadoCcc', '3'],
+		['radioIbanCompleto', '0'],
+		['txtValorDcto_0', '0,00'],
+		['txtValorDcto_1', '0,00'],
+		['hidNumChecksDctos', '2'],
+		['txtOrden', '1'],
+		['ckbVisible', '1']
+	]);
 
 	const options = {
 		method: 'POST',
@@ -163,7 +167,7 @@ async function fetchNewClientID(auth) {
 		redirect: 'manual'
 	};
 
-	options.body = form;
+	options.body = formData;
 
 	try {
 		const response = await fetch(url, options);
@@ -188,3 +192,181 @@ async function fetchNewClientID(auth) {
 		console.error(error);
 	}
 }
+
+async function insertClient(auth, clients) {
+	let output = [];
+	clients.forEach((cliente) => {
+		//FIRST Create Client
+		//SECOND Post Client Data
+		//THIRD Create Vehicle
+		//FORTH Assign Vehicle to Client
+		//FIFTH Return data with IDs of vehicles and client ID
+		console.log(cliente.nombre);
+	});
+
+	// const url = 'https://app.gdtaller.com/app/app_contactos_form.php?cod=-1&tipo=24';
+	// const formData = new URLSearchParams([
+	// 	['txtNombre', nombre],
+	// 	['txtApellidos', apellidos],
+	// 	['txtAlias', alias],
+	// 	['txtCif', cif],
+	// 	['txtTelefono', telefono1],
+	// 	['txtTelefono2', telefono2],
+	// 	['txtMovil', movil],
+	// 	['txtFax', fax],
+	// 	['txtEmail', email1],
+	// 	['txtEmail2', email2],
+	// 	['txtWeb', web],
+	// 	['selecPais', '61'],
+	// 	['selecProvincia', '-1'],
+	// 	['txtCodigoPostal', cpostal],
+	// 	['txtLocalidad', localidad],
+	// 	['txtDomicilio', direccion],
+	// 	['txtReferencia', referencia],
+	// 	['txtNombreEntrega', ''],
+	// 	['txtCnae', ''],
+	// 	['txtPersonaContacto', ''],
+	// 	['hidReferenciaAnterior', ''],
+	// 	['txtVat', ''],
+	// 	['txtValorDcto_0', '0.00'],
+	// 	['txtValorDcto_1', '0.00'],
+	// 	['hidNumChecksDctos', '2'],
+	// 	['', ''],
+	// 	['', ''],
+	// 	['', ''],
+	// 	['', ''],
+	// 	['', ''],
+	// 	['', ''],
+	// 	['', ''],
+	// 	['hidGMapAsignar', ''],
+	// 	['hidGMapGeocodeAnt', ''],
+	// 	['txtGMapDireccion', ''],
+	// 	['hidGMapLat', ''],
+	// 	['hidGMapLong', ''],
+	// 	['txtCifEntrega', ''],
+	// 	['hidGMapDesmarcar', ''],
+	// 	['hidGuardado', 'o'],
+	// 	['eCodigo', '-1'],
+	// 	['hidInfoExtra', ''],
+	// 	['BtnModificar_btn', ''],
+	// 	['BtnModificar', ''],
+	// 	['BtnEliminar', ''],
+	// 	['BtnCrear_btn', 'Crear Contacto'],
+	// 	['BtnCrear', 'Crear Contacto'],
+	// 	['selecInstalacion', '2124'],
+	// 	['txtDctoArticulos', ''],
+	// 	['txtDctoServicios', ''],
+	// 	['selecComercial', '-1'],
+	// 	['hidIdTipoSelec', '-1'],
+	// 	['txtCoefPuntosCoste', ''],
+	// 	['txtDescuentoCoste', ''],
+	// 	['txtCoefPuntosVenta', ''],
+	// 	['selecTarifaDef', '-1'],
+	// 	['hidTab', '1'],
+	// 	['ckbTipo_0', '24'],
+	// 	['eNumChecks', '3'],
+	// 	['selecSector', '-1'],
+	// 	['hidVariasActividades', 'si'],
+	// 	['eNumActividades', '0'],
+	// 	['ckbRecibirPublicidad', 'on'],
+
+	// 	['ckbUsarGMap', 'on'],
+	// 	['hidUsarGMap', '0'],
+	// 	['hidGMapZoom', '0'],
+	// 	['hidGMapAsignar', 'si'],
+	// 	['hidGMapLat', '0'],
+	// 	['hidGMapLong', '0'],
+	// 	['selecPaisEntrega', '61'],
+	// 	['selecProvinciaEntrega', '-1'],
+	// 	['radioPostalTitular', '1'],
+	// 	['radioPostalDomicilio', '1'],
+	// 	['selecFormaPago', '-1'],
+	// 	['selecVencimiento', '-1'],
+	// 	['selecDiaPago', '0'],
+	// 	['selecDiaPago2', '0'],
+	// 	['hidEstadoCcc', '3'],
+	// 	['radioIbanCompleto', '0'],
+	// 	['txtValorDcto_0', '0,00'],
+	// 	['txtValorDcto_1', '0,00'],
+	// 	['hidNumChecksDctos', '2'],
+	// 	['txtOrden', '1'],
+	// 	['ckbVisible', '1']
+	// ]);
+
+	// const options = {
+	// 	method: 'POST',
+	// 	headers: {
+	// 		authority: 'app.gdtaller.com',
+	// 		accept:
+	// 			'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8',
+	// 		'accept-language': 'en-US,en;q=0.8',
+	// 		'cache-control': 'max-age=0',
+	// 		cookie: auth,
+	// 		dnt: '1',
+	// 		origin: 'https://app.gdtaller.com',
+	// 		referer: 'https://app.gdtaller.com/app/app_contactos_form.php?cod=-1&tipo=-1',
+	// 		'sec-ch-ua': '"Not_A Brand";v="8", "Chromium";v="120", "Brave";v="120"',
+	// 		'sec-ch-ua-mobile': '?0',
+	// 		'sec-ch-ua-platform': '"Windows"',
+	// 		'sec-fetch-dest': 'document',
+	// 		'sec-fetch-mode': 'navigate',
+	// 		'sec-fetch-site': 'same-origin',
+	// 		'sec-fetch-user': '?1',
+	// 		'sec-gpc': '1',
+	// 		'upgrade-insecure-requests': '1',
+	// 		'user-agent':
+	// 			'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36'
+	// 	},
+	// 	redirect: 'manual'
+	// };
+
+	// options.body = formData;
+
+	// try {
+	// 	const response = await fetch(url, options);
+	// 	// const data = await response.json();
+	// 	// console.log(data);
+	// 	if (response.status !== 302) {
+	// 		throw new Error('Network response status was ' + response.status);
+	// 	}
+	// 	if (response.headers.has('Location')) {
+	// 		const location = response.headers.get('Location');
+	// 		console.log('Redirect URL:', location);
+	// 		const codValue = new URLSearchParams(new URL(location, 'https://dummy.com').search).get(
+	// 			'cod'
+	// 		);
+	// 		console.log(codValue);
+	// 		return { codValue: codValue, clientID: clientID };
+	// 	} else {
+	// 		// Handle other response scenarios
+	// 		console.log('No redirect location found');
+	// 	}
+	// } catch (error) {
+	// 	console.error(error);
+	// }
+}
+
+// fetch('https://app.gdtaller.com/app/app_contactos_form.php?cod=1111965&tipo=24', {
+// 	headers: {
+// 		accept:
+// 			'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8',
+// 		'accept-language': 'en-US,en;q=0.6',
+// 		'cache-control': 'max-age=0',
+// 		'content-type': 'multipart/form-data; boundary=----WebKitFormBoundarySHkJymVRIw5F53Rf',
+// 		'sec-ch-ua': '"Not_A Brand";v="8", "Chromium";v="120", "Brave";v="120"',
+// 		'sec-ch-ua-mobile': '?0',
+// 		'sec-ch-ua-platform': '"Windows"',
+// 		'sec-fetch-dest': 'document',
+// 		'sec-fetch-mode': 'navigate',
+// 		'sec-fetch-site': 'same-origin',
+// 		'sec-fetch-user': '?1',
+// 		'sec-gpc': '1',
+// 		'upgrade-insecure-requests': '1'
+// 	},
+// 	referrer: 'https://app.gdtaller.com/app/app_contactos_form.php?cod=1111965&tipo=24',
+// 	referrerPolicy: 'strict-origin-when-cross-origin',
+// 	body: '------WebKitFormBoundarySHkJymVRIw5F53Rf\r\nContent-Disposition: form-data; name="hidGuardado"\r\n\r\nno\r\n------WebKitFormBoundarySHkJymVRIw5F53Rf\r\nContent-Disposition: form-data; name="eCodigo"\r\n\r\n1111965\r\n------WebKitFormBoundarySHkJymVRIw5F53Rf\r\nContent-Disposition: form-data; name="hidInfoExtra"\r\n\r\n\r\n------WebKitFormBoundarySHkJymVRIw5F53Rf\r\nContent-Disposition: form-data; name="BtnModificar_btn"\r\n\r\nGuardar\r\n------WebKitFormBoundarySHkJymVRIw5F53Rf\r\nContent-Disposition: form-data; name="BtnModificar"\r\n\r\nGuardar\r\n------WebKitFormBoundarySHkJymVRIw5F53Rf\r\nContent-Disposition: form-data; name="BtnEliminar"\r\n\r\n\r\n------WebKitFormBoundarySHkJymVRIw5F53Rf\r\nContent-Disposition: form-data; name="selecInstalacion"\r\n\r\n2124\r\n------WebKitFormBoundarySHkJymVRIw5F53Rf\r\nContent-Disposition: form-data; name="txtDctoArticulos"\r\n\r\n\r\n------WebKitFormBoundarySHkJymVRIw5F53Rf\r\nContent-Disposition: form-data; name="txtDctoServicios"\r\n\r\n\r\n------WebKitFormBoundarySHkJymVRIw5F53Rf\r\nContent-Disposition: form-data; name="selecComercial"\r\n\r\n-1\r\n------WebKitFormBoundarySHkJymVRIw5F53Rf\r\nContent-Disposition: form-data; name="txtApellidos"\r\n\r\naplellidos\r\n------WebKitFormBoundarySHkJymVRIw5F53Rf\r\nContent-Disposition: form-data; name="hidIdTipoSelec"\r\n\r\n24\r\n------WebKitFormBoundarySHkJymVRIw5F53Rf\r\nContent-Disposition: form-data; name="txtNombre"\r\n\r\nnombre\r\n------WebKitFormBoundarySHkJymVRIw5F53Rf\r\nContent-Disposition: form-data; name="txtCoefPuntosCoste"\r\n\r\n\r\n------WebKitFormBoundarySHkJymVRIw5F53Rf\r\nContent-Disposition: form-data; name="txtDescuentoCoste"\r\n\r\n\r\n------WebKitFormBoundarySHkJymVRIw5F53Rf\r\nContent-Disposition: form-data; name="txtCoefPuntosVenta"\r\n\r\n\r\n------WebKitFormBoundarySHkJymVRIw5F53Rf\r\nContent-Disposition: form-data; name="selecTarifaDef"\r\n\r\n-1\r\n------WebKitFormBoundarySHkJymVRIw5F53Rf\r\nContent-Disposition: form-data; name="txtAlias"\r\n\r\nalias\r\n------WebKitFormBoundarySHkJymVRIw5F53Rf\r\nContent-Disposition: form-data; name="txtNombreEntrega"\r\n\r\ncombre comercial\r\n------WebKitFormBoundarySHkJymVRIw5F53Rf\r\nContent-Disposition: form-data; name="txtCnae"\r\n\r\n\r\n------WebKitFormBoundarySHkJymVRIw5F53Rf\r\nContent-Disposition: form-data; name="hidTab"\r\n\r\n1\r\n------WebKitFormBoundarySHkJymVRIw5F53Rf\r\nContent-Disposition: form-data; name="ckbTipo_0"\r\n\r\n24\r\n------WebKitFormBoundarySHkJymVRIw5F53Rf\r\nContent-Disposition: form-data; name="eNumChecks"\r\n\r\n3\r\n------WebKitFormBoundarySHkJymVRIw5F53Rf\r\nContent-Disposition: form-data; name="selecSector"\r\n\r\n-1\r\n------WebKitFormBoundarySHkJymVRIw5F53Rf\r\nContent-Disposition: form-data; name="hidVariasActividades"\r\n\r\nsi\r\n------WebKitFormBoundarySHkJymVRIw5F53Rf\r\nContent-Disposition: form-data; name="eNumActividades"\r\n\r\n0\r\n------WebKitFormBoundarySHkJymVRIw5F53Rf\r\nContent-Disposition: form-data; name="txtPersonaContacto"\r\n\r\n\r\n------WebKitFormBoundarySHkJymVRIw5F53Rf\r\nContent-Disposition: form-data; name="txtTelefono"\r\n\r\ntelefono 1\r\n------WebKitFormBoundarySHkJymVRIw5F53Rf\r\nContent-Disposition: form-data; name="txtTelefono2"\r\n\r\ntelefono 2\r\n------WebKitFormBoundarySHkJymVRIw5F53Rf\r\nContent-Disposition: form-data; name="txtMovil"\r\n\r\nmovil\r\n------WebKitFormBoundarySHkJymVRIw5F53Rf\r\nContent-Disposition: form-data; name="txtFax"\r\n\r\nfax\r\n------WebKitFormBoundarySHkJymVRIw5F53Rf\r\nContent-Disposition: form-data; name="txtEmail"\r\n\r\nemail 1\r\n------WebKitFormBoundarySHkJymVRIw5F53Rf\r\nContent-Disposition: form-data; name="txtEmail2"\r\n\r\nemail 2\r\n------WebKitFormBoundarySHkJymVRIw5F53Rf\r\nContent-Disposition: form-data; name="txtWeb"\r\n\r\nhttp://webdeltio\r\n------WebKitFormBoundarySHkJymVRIw5F53Rf\r\nContent-Disposition: form-data; name="txtCif"\r\n\r\ncif\r\n------WebKitFormBoundarySHkJymVRIw5F53Rf\r\nContent-Disposition: form-data; name="txtReferencia"\r\n\r\nref/cod\r\n------WebKitFormBoundarySHkJymVRIw5F53Rf\r\nContent-Disposition: form-data; name="hidReferenciaAnterior"\r\n\r\n\r\n------WebKitFormBoundarySHkJymVRIw5F53Rf\r\nContent-Disposition: form-data; name="txtVat"\r\n\r\n\r\n------WebKitFormBoundarySHkJymVRIw5F53Rf\r\nContent-Disposition: form-data; name="selecPais"\r\n\r\n61\r\n------WebKitFormBoundarySHkJymVRIw5F53Rf\r\nContent-Disposition: form-data; name="selecProvincia"\r\n\r\n32\r\n------WebKitFormBoundarySHkJymVRIw5F53Rf\r\nContent-Disposition: form-data; name="txtLocalidad"\r\n\r\nlocalidad\r\n------WebKitFormBoundarySHkJymVRIw5F53Rf\r\nContent-Disposition: form-data; name="txtCodigoPostal"\r\n\r\ncp\r\n------WebKitFormBoundarySHkJymVRIw5F53Rf\r\nContent-Disposition: form-data; name="txtDomicilio"\r\n\r\ndomicilio\r\n------WebKitFormBoundarySHkJymVRIw5F53Rf\r\nContent-Disposition: form-data; name="ckbUsarGMap"\r\n\r\non\r\n------WebKitFormBoundarySHkJymVRIw5F53Rf\r\nContent-Disposition: form-data; name="hidUsarGMap"\r\n\r\n0\r\n------WebKitFormBoundarySHkJymVRIw5F53Rf\r\nContent-Disposition: form-data; name="hidGMapZoom"\r\n\r\n0\r\n------WebKitFormBoundarySHkJymVRIw5F53Rf\r\nContent-Disposition: form-data; name="hidGMapDesmarcar"\r\n\r\n\r\n------WebKitFormBoundarySHkJymVRIw5F53Rf\r\nContent-Disposition: form-data; name="hidGMapAsignar"\r\n\r\nsi\r\n------WebKitFormBoundarySHkJymVRIw5F53Rf\r\nContent-Disposition: form-data; name="hidGMapGeocodeAnt"\r\n\r\n\r\n------WebKitFormBoundarySHkJymVRIw5F53Rf\r\nContent-Disposition: form-data; name="txtGMapDireccion"\r\n\r\n, localidad\r\n------WebKitFormBoundarySHkJymVRIw5F53Rf\r\nContent-Disposition: form-data; name="hidGMapLat"\r\n\r\n0\r\n------WebKitFormBoundarySHkJymVRIw5F53Rf\r\nContent-Disposition: form-data; name="hidGMapLong"\r\n\r\n0\r\n------WebKitFormBoundarySHkJymVRIw5F53Rf\r\nContent-Disposition: form-data; name="txtCifEntrega"\r\n\r\n\r\n------WebKitFormBoundarySHkJymVRIw5F53Rf\r\nContent-Disposition: form-data; name="selecPaisEntrega"\r\n\r\n61\r\n------WebKitFormBoundarySHkJymVRIw5F53Rf\r\nContent-Disposition: form-data; name="selecProvinciaEntrega"\r\n\r\n-1\r\n------WebKitFormBoundarySHkJymVRIw5F53Rf\r\nContent-Disposition: form-data; name="txtLocalidadEntrega"\r\n\r\n\r\n------WebKitFormBoundarySHkJymVRIw5F53Rf\r\nContent-Disposition: form-data; name="txtCodigoPostalEntrega"\r\n\r\n\r\n------WebKitFormBoundarySHkJymVRIw5F53Rf\r\nContent-Disposition: form-data; name="txtDomicilioEntrega"\r\n\r\n\r\n------WebKitFormBoundarySHkJymVRIw5F53Rf\r\nContent-Disposition: form-data; name="radioPostalTitular"\r\n\r\n1\r\n------WebKitFormBoundarySHkJymVRIw5F53Rf\r\nContent-Disposition: form-data; name="txtNombrePostal"\r\n\r\n\r\n------WebKitFormBoundarySHkJymVRIw5F53Rf\r\nContent-Disposition: form-data; name="radioPostalDomicilio"\r\n\r\n1\r\n------WebKitFormBoundarySHkJymVRIw5F53Rf\r\nContent-Disposition: form-data; name="selecFormaPago"\r\n\r\n-1\r\n------WebKitFormBoundarySHkJymVRIw5F53Rf\r\nContent-Disposition: form-data; name="selecVencimiento"\r\n\r\n-1\r\n------WebKitFormBoundarySHkJymVRIw5F53Rf\r\nContent-Disposition: form-data; name="selecDiaPago"\r\n\r\n0\r\n------WebKitFormBoundarySHkJymVRIw5F53Rf\r\nContent-Disposition: form-data; name="selecDiaPago2"\r\n\r\n0\r\n------WebKitFormBoundarySHkJymVRIw5F53Rf\r\nContent-Disposition: form-data; name="txtVencimiento"\r\n\r\n\r\n------WebKitFormBoundarySHkJymVRIw5F53Rf\r\nContent-Disposition: form-data; name="__txtNombreBanco"\r\n\r\n\r\n------WebKitFormBoundarySHkJymVRIw5F53Rf\r\nContent-Disposition: form-data; name="hidIdBanco"\r\n\r\n\r\n------WebKitFormBoundarySHkJymVRIw5F53Rf\r\nContent-Disposition: form-data; name="hidEstadoCcc"\r\n\r\n3\r\n------WebKitFormBoundarySHkJymVRIw5F53Rf\r\nContent-Disposition: form-data; name="txtBic"\r\n\r\n\r\n------WebKitFormBoundarySHkJymVRIw5F53Rf\r\nContent-Disposition: form-data; name="radioIbanCompleto"\r\n\r\n0\r\n------WebKitFormBoundarySHkJymVRIw5F53Rf\r\nContent-Disposition: form-data; name="txtIban"\r\n\r\n\r\n------WebKitFormBoundarySHkJymVRIw5F53Rf\r\nContent-Disposition: form-data; name="txtCcc"\r\n\r\n\r\n------WebKitFormBoundarySHkJymVRIw5F53Rf\r\nContent-Disposition: form-data; name="txtCccTitular"\r\n\r\n\r\n------WebKitFormBoundarySHkJymVRIw5F53Rf\r\nContent-Disposition: form-data; name="txtSufijoIdSepa"\r\n\r\n\r\n------WebKitFormBoundarySHkJymVRIw5F53Rf\r\nContent-Disposition: form-data; name="txtBanco"\r\n\r\n\r\n------WebKitFormBoundarySHkJymVRIw5F53Rf\r\nContent-Disposition: form-data; name="txtValorDcto_0"\r\n\r\n0,00\r\n------WebKitFormBoundarySHkJymVRIw5F53Rf\r\nContent-Disposition: form-data; name="txtValorDcto_1"\r\n\r\n0,00\r\n------WebKitFormBoundarySHkJymVRIw5F53Rf\r\nContent-Disposition: form-data; name="hidNumChecksDctos"\r\n\r\n2\r\n------WebKitFormBoundarySHkJymVRIw5F53Rf\r\nContent-Disposition: form-data; name="txtFacturaECodOC"\r\n\r\n\r\n------WebKitFormBoundarySHkJymVRIw5F53Rf\r\nContent-Disposition: form-data; name="txtFacturaECodOG"\r\n\r\n\r\n------WebKitFormBoundarySHkJymVRIw5F53Rf\r\nContent-Disposition: form-data; name="txtFacturaECodUT"\r\n\r\n\r\n------WebKitFormBoundarySHkJymVRIw5F53Rf\r\nContent-Disposition: form-data; name="txtObservaciones"\r\n\r\n\r\n------WebKitFormBoundarySHkJymVRIw5F53Rf\r\nContent-Disposition: form-data; name="txtOrden"\r\n\r\n1\r\n------WebKitFormBoundarySHkJymVRIw5F53Rf\r\nContent-Disposition: form-data; name="ckbVisible"\r\n\r\n1\r\n------WebKitFormBoundarySHkJymVRIw5F53Rf\r\nContent-Disposition: form-data; name="hidActualFoto"\r\n\r\n\r\n------WebKitFormBoundarySHkJymVRIw5F53Rf\r\nContent-Disposition: form-data; name="archivoSubidoFoto"\r\n\r\n\r\n------WebKitFormBoundarySHkJymVRIw5F53Rf--\r\n',
+// 	method: 'POST',
+// 	mode: 'cors',
+// 	credentials: 'include'
+// });
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,32 +1,86 @@
 <script>
 	import { enhance } from '$app/forms';
+	import { parse } from 'svelte/compiler';
 
 	let site = '';
 	let usr = '';
 	let pss = '';
 	export let data;
 	export let form;
+	let clientOutputFile;
+	let vehicleOutputFile;
+	let json;
+
+	function handleFile(event, callback) {
+		const reader = new FileReader();
+		reader.onload = callback;
+		reader.readAsText(event.target.files[0]);
+	}
+	$: if (clientOutputFile && vehicleOutputFile) {
+		let tempClient = JSON.parse(clientOutputFile);
+		let tempVehicle = JSON.parse(vehicleOutputFile);
+		const finalArray = tempClient.map((cliente) => {
+			const match = tempVehicle.filter((vehiculo) => vehiculo.idCliente === cliente.id);
+			return {
+				...cliente,
+				vehiculo: match
+			};
+		});
+		json = JSON.stringify(finalArray, null, 2);
+	}
+
+	function processList() {
+		console.log(clientOutputFile);
+	}
 </script>
 
 <div>
 	{#await data then}
 		{#if form?.cookie}
-			Auth is <pre>{form.cookie}</pre>
-
-			{#if form?.clientID}
-				Client ID is {form.clientID}
+			<div>
+				Auth is {form.cookie}
+				<button on:click={() => (form = '')}>Reset</button>
+			</div>
+			<br />
+			<div>
 				<form method="post" action="?/insertClient" use:enhance>
-					<input type="hidden" name="auth" value={form.cookie} />
-					<input type="hidden" name="clienID" value={form.clienID} />
-
-					<button type="submit"> Create Client </button>
+					<input type="hidden" name="auth" value={'auth'} />
+					<input type="hidden" name="clientList" value={json} />
+					<label for="clientInput">Fichero Clientes</label>
+					<input
+						type="file"
+						name="clientsFile"
+						id="clientInput"
+						on:input={(event) => {
+							handleFile(event, (e) => {
+								clientOutputFile = e.target.result;
+							});
+						}}
+						accept="application/json"
+					/>
+					<label for="clientInput">Fichero Vehiculos</label>
+					<input
+						type="file"
+						name="vehiclesFile"
+						id="vehicleInput"
+						on:input={(event) => {
+							handleFile(event, (e) => {
+								vehicleOutputFile = e.target.result;
+							});
+						}}
+						accept="application/json"
+					/>
+					<button type="submit" disabled={json == undefined}> Insert Clients </button>
 				</form>
-			{:else}
-				<form method="POST" action="?/createClient" use:enhance>
-					<input type="hidden" name="auth" value={form.cookie} />
-					<button type="submit"> Insert Client </button>
-				</form>
-			{/if}
+			</div>
+			<br />
+			<div>
+				{#if form.processedClients}
+					<pre>{form.processedClients}</pre>
+				{:else if json}
+					<pre>{json}</pre>
+				{/if}
+			</div>
 		{:else}
 			<div>
 				<form method="POST" action="?/login" use:enhance>
